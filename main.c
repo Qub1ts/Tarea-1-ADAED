e60ff93079c78f1b2dc5b23324ce9b8e09a89b15
Julio Lucero <118915945+Qub1ts@users.noreply.github.com>
2023-09-29 15:12:36 -0300

    abrir archivos

--- main.c
+++ main.c
@@ -6,13 +6,12 @@ FILE* abrirArchivo(char *nombreFile) {
     FILE *archivo;
     archivo = fopen(nombreFile,"r");
     if (archivo == NULL) {
-        printf("Error al abrir el archivo %s\n",nombreFile);
+        printf("Error al abrir el archivo %s\n", nombreFile);
         return NULL;
     }
-    printf("El archivo %s se abre correctamente\n",nombreFile);
+    printf("El archivo %s se abre correctamente\n", nombreFile);
     return archivo;
 }
-
 
 int main(int argc, char* argv[]) {
 
@@ -20,12 +19,31 @@ int main(int argc, char* argv[]) {
     FILE* fp = abrirArchivo(argv[1]);
     int numero_proyectos = atoi(argv[2]);
 
-    fp = fopen(argv[1], "r");
-    if(fp == NULL){
-        printf("Error al abrir el archivo %s\n", argv[1]);
+    if (fp == NULL){
         return 0;
     }
-        
+    int proyecto[numero_proyectos], costo[numero_proyectos];
+    char costos[numero_proyectos][100];
+    int i = 0;
+    while (i < numero_proyectos) {
+        fscanf(fp, "%i %s", &proyecto[i], costos[i]);
+        i++;
+    }
+
+    fclose(fp);
+
+    // Imprimir los datos leídos
+    for (i = 0; i < numero_proyectos; i++) {
+        printf("Proyecto: %d, Costo: %s\n", proyecto[i], costos[i]);
+    }
+
+    return 0;
+}
+
+
+
+
+
 
 
 
