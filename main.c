f6c59aa014065784e1800d1505849274b66b0765
Julio Lucero <118915945+Qub1ts@users.noreply.github.com>
2023-09-29 16:04:05 -0300

    main leer archivo y estructura de proyectos

--- main.c
+++ main.c
@@ -6,44 +6,52 @@ FILE* abrirArchivo(char *nombreFile) {
     FILE *archivo;
     archivo = fopen(nombreFile,"r");
     if (archivo == NULL) {
-        printf("Error al abrir el archivo %s\n", nombreFile);
+        printf("Error al abrir el archivo %s\n",nombreFile);
         return NULL;
     }
-    printf("El archivo %s se abre correctamente\n", nombreFile);
+    printf("El archivo %s se abre correctamente\n",nombreFile);
     return archivo;
 }
 
-int main(int argc, char* argv[]) {
-
-    // ABRIR EL ARCHIVO
+int main(int argc, char *argv[]){
     FILE* fp = abrirArchivo(argv[1]);
-    int numero_proyectos = atoi(argv[2]);
 
-    if (fp == NULL){
-        return 0;
-    }
-    int proyecto[numero_proyectos], costo[numero_proyectos];
-    char costos[numero_proyectos][100];
-    int i = 0;
-    while (i < numero_proyectos) {
-        fscanf(fp, "%i %s", &proyecto[i], costos[i]);
-        i++;
-    }
+    typedef struct {
+        int sroi;
+        int costos;
+    }proyectos;
 
-    fclose(fp);
 
-    // Imprimir los datos leídos
-    for (i = 0; i < numero_proyectos; i++) {
-        printf("Proyecto: %d, Costo: %s\n", proyecto[i], costos[i]);
-    }
+    //Encontramos el maximo en este caso maximizar los sroi totales
+    void maximo()
+    
 
-    return 0;
+    // int i, numero_proyecto = atoi(argv[2]);
+
+    // int proyectos[numero_proyecto];
+    // int costos[numero_proyecto];
+
+    // i = 0;
+    // while(i < numero_proyecto){
+    //     fscanf(fp, "%i %i", &proyectos[i], &costos[i]);
+    //     i++;
+    // }
+    // fclose(fp);
+
+
+    // //Mostrar en consola lo que contiene el archivo txt
+    // i = 0;
+    // while(i < numero_proyecto){
+    //     printf("%i %i\n", proyectos[i], costos[i]);
+    //     i++;
+    // }
+    // return 0;
+
+
+
+
+
 }
-
-
-
-
-
 
 
 
