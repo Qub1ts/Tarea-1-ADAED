b5699df0886602de3f891564e7c8e1586e8efc55
Julio Lucero <118915945+Qub1ts@users.noreply.github.com>
2023-09-30 15:20:12 -0300

    solucion final

--- main.c
+++ main.c
@@ -1,48 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// ABRIR UN ARCHIVO EN MODO LECTURA
-FILE* abrirArchivo(char *nombreFile) {
-    FILE *archivo;
-    archivo = fopen(nombreFile,"r");
-    if (archivo == NULL) {
-        printf("Error al abrir el archivo %s\n",nombreFile);
-        return NULL;
-    }
-    printf("El archivo %s se abre correctamente\n",nombreFile);
-    return archivo;
-}
-
 typedef struct {
-        int sroi;
-        int costos;
-    }proyectos;
+    int sroi;
+    int costos;
+} Proyectos;
 
-void solucion(proyectos proyecto[], int n, int presupuesto_max, int *mejor_conjunto, int *maximo_sroi){
-    int total_conjuntos = 1 << n; // Sabemos cuantas combinaciones de subconjuntos de proyectos
+int leer_archivo(const char *nombrearchivo, Proyectos **proyecto, int *n, int *presupuesto_max);
+void imprimir_proyectos_seleccionados(int mejor_conjunto, Proyectos *proyecto, int n);
+int Solucion(Proyectos *proyecto, int n, int presupuesto_max);
 
-    *maximo_sroi = 0; // inicializamos el valor maximo de sroi a 0
+int main() {
+    char nombrearchivo[50];
+    Proyectos *proyecto = 0;
+    int n; 
+    int presupuesto_max;
+    int mejor_conjunto;
 
-    // Recorremos todos los conjuntos posibles
-    for(int i = 0; i < total_conjuntos; i++){
-        int actual_sroi = 0;
-        int actual_costos = 0;
-        for(int j = 0; j < n; j++){
-            // Verificamos si el proyecto j esta incluido en el subconjunto i
-            if(i & (1 << j)){
-                actual_sroi += proyecto[j].sroi;
-                actual_costos += proyecto[j].costos;
-            }               
-        }
+    printf("Ingresa el archivo (ejemplo: knapPI_6_500.txt): ");
+    scanf("%s", nombrearchivo);
 
-        // Preguntamos si los costos actuales son menores o iguales al presupuesto y 
-        // si el actual sroi es mayor al maximo
-        if(actual_costos <= presupuesto_max && actual_sroi > *maximo_sroi){
-            *maximo_sroi = actual_sroi; // actualizamos el maximo sroi
-            *mejor_conjunto = i;   // lo asignamos como el mejor conjunto
+     if (!leer_archivo(nombrearchivo, &proyecto, &n, &presupuesto_max)) {
+         printf("Error al abrir el archivo.\n");
+         free(proyecto);
+         return 1;
+     }
+
+    mejor_conjunto = Solucion(proyecto, n, presupuesto_max);
+
+    printf("\nSeleccionamos al mejor proyecto:\n");
+    imprimir_proyectos_seleccionados(mejor_conjunto, proyecto, n);
+
+    free(proyecto);
+    return 0;
+}
+
+int leer_archivo(const char *nombrearchivo, Proyectos **proyecto, int *n, int *presupuesto_max) {
+    FILE *archivo = fopen(nombrearchivo, "r");
+    if (!archivo) return 0;
+
+    sscanf(nombrearchivo, "knapPI_%d_%d.txt", n, presupuesto_max);
+    
+    *proyecto = (Proyectos*)malloc(sizeof(Proyectos) * (*n));
+    for (int i = 0; i < *n; i++) {
+        fscanf(archivo, "%d %d", &(*proyecto)[i].sroi, &(*proyecto)[i].costos);
+    }
+
+    fclose(archivo);
+    return 1;
+}
+
+void imprimir_proyectos_seleccionados(int mejor_conjunto, Proyectos *proyecto, int n) {
+    int costo_total = 0;
+    int sroi_total = 0;
+    for (int j = 0; j < n; j++) {
+        if (mejor_conjunto & (1 << j)) {
+            printf("Proyecto %d con SROI %d y costo %d\n", j + 1, proyecto[j].sroi, proyecto[j].costos);
+            sroi_total += proyecto[j].sroi;
+            costo_total += proyecto[j].costos;
         }
     }
+    printf("\nTotal SROI: %d\nTotal Cost: %d\n", sroi_total, costo_total);
 }
+
+int Solucion(Proyectos *proyecto, int n, int presupuesto_max) {
+    int subconjuntos = 1 << n;
+    int maximo_sroi = 0;
+    int mejor_conjunto = 0;
+
+    for (int i = 0; i < subconjuntos; i++) {
+        int sroi_actual = 0;
+        int costo_actual = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                sroi_actual += proyecto[j].sroi;
+                costo_actual += proyecto[j].costos;
+            }
+        }
+
+        if (costo_actual <= presupuesto_max && sroi_actual > maximo_sroi) {
+            maximo_sroi = sroi_actual;
+            mejor_conjunto = i;
+        }
+    }
+    return mejor_conjunto;
+}
+
+
+
+
+
 
 
 
